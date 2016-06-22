c69b4b1270b8bbcf75559a3dab70c02a45d3b251
Sungjin Han <meinside@gmail.com>
2016-06-22 16:10:23 +0900

    Initial import

---
+++ README.md
@@ -0,0 +1,86 @@
+# Raspberry Pi Camera: Youtube Streamer
+
+## 0. What is this?
+
+It's a go application for streaming video through [Youtube Live Stream](https://www.youtube.com/live_dashboard), using [raspivid](https://www.raspberrypi.org/documentation/usage/camera/raspicam/raspivid.md) and [ffmpeg](https://ffmpeg.org/) on [Raspberry Pi](https://www.raspberrypi.org/).
+
+## 1. What do I need before running it?
+
+You need:
+
+* Raspberry Pi
+* Raspberry Pi Camera Module enabled, and its cable correctly connected
+* [golang installed on Raspberry Pi](https://github.com/meinside/rpi-configs/blob/master/bin/prep_go.sh)
+* [ffmpeg installed on Raspberry Pi](https://github.com/meinside/rpi-configs/blob/master/bin/prep_ffmpeg.sh)
+* and this README.md.
+
+## 2. How can I build it?
+
+```bash
+$ go get -u github.com/meinside/rpi-camera-youtube-streamer
+$ cd $GOPATH/src/github.com/meinside/rpi-camera-youtube-streamer
+$ go build
+```
+
+or
+
+```bash
+$ git clone https://github.com/meinside/rpi-camera-youtube-streamer.git
+$ cd rpi-camera-youtube-streamer
+$ go build
+```
+
+## 3. How can I run it?
+
+You need to create your own config file.
+
+Sample file is included, so feel free to copy it and change values as you want.
+
+```bash
+$ cp config.json.sample config.json
+$ vi config.json
+```
+
+At least you have to change the **youtube_stream_key** value for running it.
+
+You can get your key in your [Live Stream Dashboard](https://www.youtube.com/live_dashboard), which is labeled as 'Stream name/key'.
+
+After the configuration is finished, just execute the binary:
+
+```bash
+$ ./rpi-cameera-youtube-streamer
+```
+
+If nothing goes wrong, you'll see your live streaming in your dashboard in several seconds.
+
+## 4. How can I run it as a service?
+
+### systemd
+
+```bash
+$ sudo cp systemd/rpi-camera-youtube-streamer.service /lib/systemd/system/
+$ sudo vi /lib/systemd/system/rpi-camera-youtube-streamer.service
+```
+
+and edit **User**, **Group**, **WorkingDirectory**, and **ExecStart** values.
+
+You can simply start/stop it with:
+
+```
+$ sudo systemctl start rpi-camera-youtube-streamer.service
+$ sudo systemctl stop rpi-camera-youtube-streamer.service
+```
+
+If you want to launch it automatically on boot:
+
+```bash
+$ sudo systemctl enable telegram-bot-rpi-camera.service
+```
+
+## 998. Any trouble?
+
+Please open an issue.
+
+## 999. License?
+
+MIT
---
+++ main.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"log"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"strings"
+)
+
+const (
+	ConfigFilename = "config.json"
+)
+
+type config struct {
+	YoutubeStreamKey string `json:"youtube_stream_key"`
+
+	VideoWidth    int    `json:"video_width"`
+	VideoHeight   int    `json:"video_height"`
+	VideoRotation int    `json:"video_rotation"`
+	VideoExposure string `json:"video_exposure"`
+	VideoAWB      string `json:"video_awb"`
+
+	IsVerbose bool `json:"is_verbose"`
+}
+
+var youtubeStreamKey string
+var videoWidth int
+var videoHeight int
+var videoRotation int
+var videoExposure string
+var videoAWB string
+var isVerbose bool
+
+// Read config
+func getConfig() (config, error) {
+	_, filename, _, _ := runtime.Caller(0) // = __FILE__
+
+	if file, err := ioutil.ReadFile(filepath.Join(path.Dir(filename), ConfigFilename)); err == nil {
+		var conf config
+		if err := json.Unmarshal(file, &conf); err == nil {
+			return conf, nil
+		} else {
+			return config{}, err
+		}
+	} else {
+		return config{}, err
+	}
+}
+
+func init() {
+	// read config
+	if conf, err := getConfig(); err != nil {
+		panic(err)
+	} else {
+		youtubeStreamKey = conf.YoutubeStreamKey
+		videoWidth = conf.VideoWidth
+		videoHeight = conf.VideoHeight
+		videoRotation = conf.VideoRotation
+		videoExposure = conf.VideoExposure
+		videoAWB = conf.VideoAWB
+		isVerbose = conf.IsVerbose
+	}
+}
+
+func main() {
+	// http://maxogden.com/hd-live-streaming-cats.html
+	raspividArgs := []string{
+		"-o", "-", // to STDIO
+		"-t", "0", // no timeout
+		"-fps", "30", // 30 frames/sec
+		"-b", "6000000", // 6 MBits
+
+		// read from config
+		"-w", strconv.Itoa(videoWidth), // width
+		"-h", strconv.Itoa(videoHeight), // height
+		"-rot", strconv.Itoa(videoRotation), // rotation
+		"-ex", videoExposure, // exposure
+		"-awb", videoAWB, // white balance
+	}
+	ffmpegArgs := []string{
+		"-re",
+		"-ar", "44100",
+		"-ac", "2",
+		"-acodec", "pcm_s16le",
+		"-f", "s16le",
+		"-ac", "2",
+		"-i", "/dev/zero",
+		"-f", "h264",
+		"-i", "-",
+		"-vcodec", "copy",
+		"-acodec", "aac",
+		"-ab", "128k",
+		"-g", "60", // number of frames per keyframe
+		"-strict", "experimental",
+		"-f", "flv",
+		"rtmp://a.rtmp.youtube.com/live2/" + youtubeStreamKey,
+	}
+
+	// commands
+	raspivid := exec.Command("raspivid", raspividArgs...)
+	ffmpeg := exec.Command("ffmpeg", ffmpegArgs...)
+
+	// pipe raspivid's STDOUT to ffmpeg's STDIN
+	reader, writer := io.Pipe()
+	raspivid.Stdout = writer
+	ffmpeg.Stdin = reader
+
+	// run raspivid
+	go func() {
+		defer writer.Close()
+
+		if err := raspivid.Start(); err != nil {
+			panic(err)
+		}
+		if isVerbose {
+			log.Printf("started raspivid with args: %s", strings.Join(raspividArgs, " "))
+		} else {
+			log.Printf("started raspivid")
+		}
+		if err := raspivid.Wait(); err != nil {
+			panic(err)
+		}
+	}()
+
+	// run ffmpeg
+	if err := ffmpeg.Start(); err != nil {
+		panic(err)
+	}
+	if isVerbose {
+		log.Printf("started ffmpeg with args: %s", strings.Join(ffmpegArgs, " "))
+	} else {
+		log.Printf("started ffmpeg")
+	}
+	if err := ffmpeg.Wait(); err != nil {
+		log.Printf("ffmpeg finished with error: %s", err)
+	}
+}
